4f8dbc25f3a791f3ce3d1767a40818ee2b412f91
RubenMartinezD <92438338+RubenMartinezD@users.noreply.github.com>
2021-11-11 17:42:54 +0100

    Update main.js

--- semana5/Pr5_e1/main.js
+++ semana5/Pr5_e1/main.js
@@ -7,7 +7,13 @@ salidinero()
 }
 main()
 
-/*function main() {
+/*
+function main() {  mal copiado
 var cantidad_pedida_usuario
+var cantidad_de_billetes
 cantidad_pedida_usuario=  entrda_usuario("introduce una cantidad. ")
-retirada_biletes(cantidad)
+if (multiplo_de_0_5(cantidad_pedida_usuartio))
+cantidad_de_billtes=retirada_biletes(cantidad_pedida_usuario)
+salida_por_consola(cantidad_billtes)
+
+*/
