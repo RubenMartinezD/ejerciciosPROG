21bf2a7364ab9552eb60323ca4b895a0def5b8ff
Rubén <rmartinezdominguez@danielcastelao.org>
2022-01-24 16:49:46 +0100

    ej1

---
+++ semana11/enemigo.js
@@ -0,0 +1,8 @@
+class Enemigo {
+    constructor(nombre, NV, PV, AT) {
+        this.nombre = nombre;
+        this.NV = NV;
+        this.PV = PV;
+        this.AT = AT;
+    }
+}
---
+++ semana11/main.js
@@ -0,0 +1,8 @@
+/**
+ * Función principal
+ */
+function main() {
+    cosa = new Enemigo("Rodolfo", "3", "25", "8")
+    console.log(cosa)
+}
+main()
