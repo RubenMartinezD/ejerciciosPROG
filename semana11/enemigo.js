a5968fed28a903f797ba517943df555d5116bdb8
Rubén <rmartinezdominguez@danielcastelao.org>
2022-01-24 18:42:57 +0100

    Ej 5

--- semana11/enemigo.js
+++ semana11/enemigo.js
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 class Enemigo {
     constructor(nombre, NV, PV, AT) {
         this.nombre = nombre;
@@ -6,16 +9,16 @@ class Enemigo {
         this.AT = AT;
     }
     get getnombre() {
-        return this.nombre
+        return this.nombre;
     }
     get getNV() {
-        return this.NV
+        return this.NV;
     }
     get getPV() {
-        return this.PV
+        return this.PV;
     }
     get getAT() {
-        return this.AT
+        return this.AT;
     }
     set setnombre(nombre) {
         this.nombre = nombre;
@@ -29,13 +32,52 @@ class Enemigo {
     set setAT(AT) {
         this.AT = AT;
     }
+
+    /**
+     * 
+     * @param {integer} daño_recibido -- Daño recibido por el enemigo
+     */
     recibirDaño(daño_recibido) {
-        this.PV = (this.PV - daño_recibido)
-        console.log(this.nombre + " recibió " + daño_recibido + " puntos de daño. Su salud actual es de " + this.PV + " PV.")
+        this.PV = (this.PV - daño_recibido);
+        console.log(this.nombre + " recibió " + daño_recibido + " puntos de daño. Su salud actual es de " + this.PV + " PV.");
     }
+
+    /**
+     * Función para calcular la experiencia obtenida
+     * @param {integer} valor_exp - Variable para ajustar el nivel de experiencia
+     */
     recibirExp(valor_exp) {
-        this.valor_exp = parseInt(valor_exp)
-        let puntos_exp = ((this.NV * 5) / this.valor_exp)
-        console.log("Al matar al " + this.nombre + " se reciben " + puntos_exp + " puntos de experiencia.")
+        this.valor_exp = parseInt(valor_exp);
+        let puntos_exp = ((this.NV * 5) / this.valor_exp);
+        console.log("Al matar al " + this.nombre + " se reciben " + puntos_exp + " puntos de experiencia.");
+    }
+
+    /**
+     * Determinar si el enemigo está vivo o no. Cuando esté muerto mantener su valor de PV en 0.
+     * @returns {boolean} vivo 
+     */
+    viveEnemigo() {
+        let vivo;
+        if (this.PV < 0) {
+            this.PV == 0
+            vivo = false;
+        } else if (this.PV == 0) {
+            vivo = false;
+        } else {
+            vivo = true;
+        }
+        return vivo;
+    }
+
+    /**
+     * Devuelve un mensaje en función de si el enemigo se encuentra vivo o muerto.
+     */
+    viveMensaje() {
+        let estado_vivo = this.viveEnemigo()
+        if (estado_vivo == true) {
+            console.log(this.nombre + " se encuentra vivo.")
+        } else {
+            console.log(this.nombre + " se encuentra muerto.")
+        }
     }
 }
--- semana11/main.js
+++ semana11/main.js
@@ -9,8 +9,9 @@ function main() {
     cosa.setNV = 15;
     cosa.setAT = 666;
     console.log("El nivel de " + cosa.getnombre + " es " + cosa.getNV + ". Su salud actual es de " + cosa.getPV + " puntos y su poder de ataque es de " + cosa.getAT)
-    cosa.recibirDaño(5)
-    console.log("El nivel de " + cosa.getnombre + " es " + cosa.getNV + ". Su salud actual es de " + cosa.getPV + " puntos y su poder de ataque es de " + cosa.getAT)
-    cosa.recibirExp(4) //mas alto = menos Exp.
+    cosa.viveMensaje();
+    cosa.recibirDaño(55);
+    cosa.recibirExp(4); //mas alto = menos Exp.
+    cosa.viveMensaje();
 }
 main()
